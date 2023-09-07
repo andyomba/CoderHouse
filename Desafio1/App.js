d85e9caec351ad6d1b2fdf717b6874c3674e0b3c
andyomba <andit996@gmail.com>
2023-09-07 00:04:21 -0300

    act

--- Desafio1/App.js
+++ Desafio1/App.js
@@ -1,72 +1,135 @@
-import { StatusBar } from 'expo-status-bar';
-import { View, TextInput, Button, StyleSheet, Text } from 'react-native';
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native'
+import React, { useState } from 'react'
+
+import Modal from './components/Modal'
 
 export default function App() {
-    return (
-        <View style={{padding: 30}}>
-            <View style={styles.inputContainer}>
-                <TextInput
-                    placeholder='Item de la lista'
-                    placeholderTextColor={"#ff0000"}
-                    style={styles.input}
-                />
-                <Button title='ADD' color={"#ff0000"} />
-            </View>
-            <View style={styles.listContainer}>
+  const [textValue, setTextValue] = useState('')
+  const [itemsList, setItemsList] = useState([])
+  const [itemSelected, setItemSelected] = useState()
+  const [modalVisible, setModalVisible] = useState(false)
 
-                <View style={styles.TextContainer}>
-                    <Text style={styles.Text}>Item 1</Text>
-                </View>
+  const onHandleChangeItem = text => setTextValue(text)
 
-                <View style={styles.TextContainer}>
-                    <Text style={styles.Text}>Item 2</Text>
-                </View>
+  const addItem = () => {
+    if (textValue === '') {
+      return
+    }
+    console.log('ejecuta la funcion de agregar elemento')
+    setItemsList(prevState => [
+      ...prevState,
+      { id: Math.random(), value: textValue },
+    ])
+    setTextValue('')
+  }
 
-                <View style={styles.TextContainer}>
-                    <Text style={styles.Text}>Item 3</Text>
-                </View>
+  const renderListItem = ({ item, index }) => (
+    <TouchableOpacity
+      style={styles.itemContainer}
+      onPress={() => onHandleModal(index)}
+    >
+      <Text style={styles.textItem}>{item?.value}</Text>
+    </TouchableOpacity>
+  )
 
-            </View>
-        </View>
-    );
-};
+  const onHandleDelete = () => {
+    console.log('se esta eliminando el elemento: '+itemSelected)
+    let arr = itemsList
+    arr.splice(itemSelected, 1)
+    setItemsList(arr)
+    setModalVisible(false)
+  }
+  
+
+  const onHandleModal = index => {
+    setModalVisible(true)
+    setItemSelected(index)
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Barber List</Text>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="New product..."
+          value={textValue}
+          onChangeText={onHandleChangeItem}
+        />
+        <Button title="Add" color={'#007BFF'} onPress={addItem} />
+      </View>
+      <View style={styles.listContainer}>
+        <FlatList
+          data={itemsList}
+          renderItem={renderListItem}
+          keyExtractor={item => item.id}
+        />
+      </View>
+      <Modal modalVisible={modalVisible} onHandleDelete={onHandleDelete} />
+    </View>
+  )
+}
+
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 30
-    },
-    inputContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-    },
-    input: {
-        borderBottomColor: "#ff0000",
-        borderBottomWidth: 1,
-        width: 200,
-        height: 50,
-        color: "#ff0000",
-        fontSize: 20,
-        paddingLeft: 12,
-    },
-    listContainer: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderColor: "#ff0000",
-        borderWidth: 2,
-        marginTop: 40,
-    },
-    TextContainer: {
-        borderColor: "#ff0000",
-        alignItems: 'center',
-        borderWidth: 2,
-        marginVertical: 20,
-        padding: 10,
-        width: "90%",
-    },
-    Text: {
-        fontSize: 24,
-        color: "#ff0000",
-        fontWeight: 'bold',
-    }
-})
+  container: {
+    flex: 1,
+    padding: 30,
+    paddingTop: 20,
+    backgroundColor: '#87CEEB',
+  },
+  title: {
+    fontSize: 35,
+    fontWeight: '700',
+    marginBottom: 10,
+  },
+  inputContainer: {
+    borderRadius: 10,
+    alignItems: 'center',
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    justifyContent: 'space-between',
+  },
+  input: {
+    width: 200,
+    height: 50,
+    fontSize: 17,
+    paddingLeft: 12,
+  },
+  listContainer: {
+    marginTop: 25,
+    marginBottom: 80,
+  },
+  itemContainer: {
+    height: 40,
+    marginVertical: 10,
+    marginHorizontal: 5,
+    borderRadius: 10,
+    justifyContent: 'center',
+    backgroundColor: '#007',
+    shadowColor: '#fe6855',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+    elevation: 5,
+  },
+  textItem: {
+    fontSize: 18,
+    paddingLeft: 15,
+    color: '#fff',
+    fontWeight: '600',
+    fontVariant: 'no-common-ligatures',
+  },
+  modalButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
