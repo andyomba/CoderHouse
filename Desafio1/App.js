349a93a35b14d928cb8179dda6e220128d19ec17
andyomba <andit996@gmail.com>
2023-09-06 23:57:38 -0300

    act

--- Desafio1/App.js
+++ Desafio1/App.js
@@ -1,36 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, TextInput, Button, StyleSheet, Text } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.header}>Primer desafio!!!</Text>
-      <Text style={styles.text}>Hola Coder XD</Text>
-      <Text style={styles.footer}>listo para el proximo...</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    return (
+        <View style={{padding: 30}}>
+            <View style={styles.inputContainer}>
+                <TextInput
+                    placeholder='Item de la lista'
+                    placeholderTextColor={"#ff0000"}
+                    style={styles.input}
+                />
+                <Button title='ADD' color={"#ff0000"} />
+            </View>
+            <View style={styles.listContainer}>
+
+                <View style={styles.TextContainer}>
+                    <Text style={styles.Text}>Item 1</Text>
+                </View>
+
+                <View style={styles.TextContainer}>
+                    <Text style={styles.Text}>Item 2</Text>
+                </View>
+
+                <View style={styles.TextContainer}>
+                    <Text style={styles.Text}>Item 3</Text>
+                </View>
+
+            </View>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FF5733',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  header: {
-    fontSize: 34,
-    color: 'white',
-    marginBottom: 10,
-  },
-  text: {
-    fontSize: 24,
-    color: 'black',
-  },
-  footer: {
-    fontSize: 18,
-    color: 'white',
-    marginTop: 500,
-  },
-});
+    container: {
+        padding: 30
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    input: {
+        borderBottomColor: "#ff0000",
+        borderBottomWidth: 1,
+        width: 200,
+        height: 50,
+        color: "#ff0000",
+        fontSize: 20,
+        paddingLeft: 12,
+    },
+    listContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderColor: "#ff0000",
+        borderWidth: 2,
+        marginTop: 40,
+    },
+    TextContainer: {
+        borderColor: "#ff0000",
+        alignItems: 'center',
+        borderWidth: 2,
+        marginVertical: 20,
+        padding: 10,
+        width: "90%",
+    },
+    Text: {
+        fontSize: 24,
+        color: "#ff0000",
+        fontWeight: 'bold',
+    }
+})
