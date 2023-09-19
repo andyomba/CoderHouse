b5336dc1f414e9ab9d3d40d511f11b99a5a3bbdc
andyomba <andit996@gmail.com>
2023-09-18 23:58:53 -0300

    act

--- Desafio1/App.js
+++ Desafio1/App.js
@@ -1,135 +1,15 @@
-import {
-  Button,
-  FlatList,
-  StyleSheet,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from 'react-native'
-import React, { useState } from 'react'
+import { Home, Products } from './src/screens'
 
-import Modal from './components/Modal'
+import fonts from './src/global/fonts'
+import { useState } from 'react'
+import { useFonts } from 'expo-font'
+
 
 export default function App() {
-  const [textValue, setTextValue] = useState('')
-  const [itemsList, setItemsList] = useState([])
-  const [itemSelected, setItemSelected] = useState()
-  const [modalVisible, setModalVisible] = useState(false)
+  const [fontsLoaded] = useFonts(fonts)
+  const[categorySelected , setCategorySelected] = useState('')
 
-  const onHandleChangeItem = text => setTextValue(text)
+  if (!fontsLoaded){return null}
 
-  const addItem = () => {
-    if (textValue === '') {
-      return
-    }
-    console.log('ejecuta la funcion de agregar elemento')
-    setItemsList(prevState => [
-      ...prevState,
-      { id: Math.random(), value: textValue },
-    ])
-    setTextValue('')
-  }
-
-  const renderListItem = ({ item, index }) => (
-    <TouchableOpacity
-      style={styles.itemContainer}
-      onPress={() => onHandleModal(index)}
-    >
-      <Text style={styles.textItem}>{item?.value}</Text>
-    </TouchableOpacity>
-  )
-
-  const onHandleDelete = () => {
-    console.log('se esta eliminando el elemento: '+itemSelected)
-    let arr = itemsList
-    arr.splice(itemSelected, 1)
-    setItemsList(arr)
-    setModalVisible(false)
-  }
-  
-
-  const onHandleModal = index => {
-    setModalVisible(true)
-    setItemSelected(index)
-  }
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Barber List</Text>
-      <View style={styles.inputContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="New product..."
-          value={textValue}
-          onChangeText={onHandleChangeItem}
-        />
-        <Button title="Add" color={'#007BFF'} onPress={addItem} />
-      </View>
-      <View style={styles.listContainer}>
-        <FlatList
-          data={itemsList}
-          renderItem={renderListItem}
-          keyExtractor={item => item.id}
-        />
-      </View>
-      <Modal modalVisible={modalVisible} onHandleDelete={onHandleDelete} />
-    </View>
-  )
+  return categorySelected ? <Products category={categorySelected} /> : ( <Home setCategorySelected={setCategorySelected} />)
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 30,
-    paddingTop: 20,
-    backgroundColor: '#87CEEB',
-  },
-  title: {
-    fontSize: 35,
-    fontWeight: '700',
-    marginBottom: 10,
-  },
-  inputContainer: {
-    borderRadius: 10,
-    alignItems: 'center',
-    flexDirection: 'row',
-    backgroundColor: '#fff',
-    justifyContent: 'space-between',
-  },
-  input: {
-    width: 200,
-    height: 50,
-    fontSize: 17,
-    paddingLeft: 12,
-  },
-  listContainer: {
-    marginTop: 25,
-    marginBottom: 80,
-  },
-  itemContainer: {
-    height: 40,
-    marginVertical: 10,
-    marginHorizontal: 5,
-    borderRadius: 10,
-    justifyContent: 'center',
-    backgroundColor: '#007',
-    shadowColor: '#fe6855',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.5,
-    shadowRadius: 3,
-    elevation: 5,
-  },
-  textItem: {
-    fontSize: 18,
-    paddingLeft: 15,
-    color: '#fff',
-    fontWeight: '600',
-    fontVariant: 'no-common-ligatures',
-  },
-  modalButtons: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-});
